157cfd317d56761b4e229f24ffe9590939aba8f4
takayuki <takayuki@idein.jp>
2018-04-03 19:06:49 -0700

    sync

--- build.rs
+++ build.rs
@@ -4,7 +4,25 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 fn main() {
-    println!("cargo:rustc-link-lib=qmkl");
+    //println!("cargo:rustc-link-lib=qmkl");
+
+    // Path to directories of C header
+    let include_dirs: Vec<PathBuf> = vec![
+        //"/usr/include/clang/3.9.1/include".into(),
+        "/home/idein/x-tools/armv6-rpi-linux-gnueabihf/armv6-rpi-linux-gnueabihf/sysroot/usr/include".into(),
+        "/home/idein/x-tools/armv6-rpi-linux-gnueabihf/lib/gcc/armv6-rpi-linux-gnueabihf/6.3.1/include".into(),
+        "/home/idein/cross/usr/armv6-rpi-linux-gnueabihf/include".into(),
+        /*
+        Path::new(&env::var("C_INCLUDE_PATH")
+            .expect("C_INCLUDE_PATH like: /usr/lib/llvm-3.9/lib/clang/3.9.1/include"))
+            .into(),
+            */
+    ];
+
+    let include_args: Vec<_> = include_dirs
+        .iter()
+        .flat_map(|path| vec!["-I", path.to_str().unwrap()])
+        .collect();
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -13,6 +31,9 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .link("qmkl")
+        .clang_args(&["-L", "/home/idein/cross/usr/armv6-rpi-linux-gnueabihf/lib"])
+        .clang_args(&include_args)
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
